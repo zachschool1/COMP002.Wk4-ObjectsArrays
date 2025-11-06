b613e2adba5e2c941017521a3444e1ce152f0544
Zachary Jordan <zachary.jordan@my.whccd.edu>
2025-11-06 00:00:02 -0800

    fixed range because it wasnt making it an array and then finished #2 since it required an array as an input

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 // 1. Write a range function that takes two arguments, start and end, and returns an array
 // containing all the numbers from start up to (and including) end.
 // Example output:
-// console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 function range(start,end) {
+    let answer = [];
+    //could probably do a while loop where while start <= end console.log i and i++ but i like for loop
     for(let i = start; i <= end; i++) {
-        console.log(i);
+        //console.log(i);
+        answer.push(i);
     }
+    return answer;
 }
-
-range(1,10);
 
 // 2. Next, write a sum function that takes an array of numbers and returns the sum of
 // these numbers. Run the example program and see whether it does indeed return 55.
 // Example output:
-// console.log(sum(range(1, 10))); // returns 55
+console.log(sum(range(1, 10))); // returns 55
+function sum(array){
+    let total = 0;
+        for(let i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    return total;
+}
+
+
+
 
 // 3. Arrays have a reverse method that changes the array by inverting the order in which
 // its elements appear. For this exercise, write two functions, reverseArray and
